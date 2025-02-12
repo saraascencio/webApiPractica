8a014cce4a928940881975a33c7f9847d4f12228
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-02-11 22:18:44 -0600

    Cambios guía parte 2

--- WebApipractica/Controllers/equiposController.cs
+++ WebApipractica/Controllers/equiposController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebApipractica.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApipractica.Controllers
 {
@@ -15,6 +17,7 @@ namespace WebApipractica.Controllers
             _equiposContexto = equiposContexto;
         }
 
+        //Obtener todos los elementos de la lista
         [HttpGet]
         [Route("GetAll")]
         public IActionResult Get()
@@ -28,6 +31,119 @@ namespace WebApipractica.Controllers
             }
             return Ok(listadoEquipo);
         }
+        // Filtrar elementos y obtenerlos mediante
+        // una llave principal
+
+        [HttpGet]
+        [Route(("GetById/{id}"))]
+        public IActionResult Get(int id)
+        {
+
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+
+        }
+
+        //Filtrado de búsqueda mediante un campo string
+        [HttpGet]
+        [Route("Find/{filtro}")]
+        public IActionResult FindByDescription(string filtro)
+        {
+
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+
+        }
+
+        //Metodo para guardar el registro 
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+
+            try
+            {
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        //Metodo para modificar
+        [HttpPut]
+        [Route("actualizar/{id}")]
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+
+            //Actualizar registro original de BD
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                                     where e.id_equipos == id
+                                     select e).FirstOrDefault();
+
+            //Verificamos que exista el registro
+            if (equipoActual == null)
+            { return NotFound(); }
+
+            //Si se encuentra el registro se alteran los campos modificables
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoActual.anio_compra = equipoModificar.anio_compra;
+            equipoActual.costo = equipoModificar.costo;
+
+            //Se marca el registro como modificado y se envia la notificacion 
+            // a la BD ****************
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipoModificar);
+
+        }
+
+        //Metodo para eliminar 
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+            //Se obtiene el registro que se desea actualizar
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+                return NotFound();
+
+            _equiposContexto.equipos.Attach(equipo);
+            _equiposContexto.equipos.Remove(equipo);
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipo);
+
+        }
+
+ 
 
     }
 }
